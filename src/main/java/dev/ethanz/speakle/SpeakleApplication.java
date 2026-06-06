16c2cb47f371935e1f1b3dd2431343106290c8b5
Ethan Zhang <dangerwindow@gmail.com>
2026-06-05 20:15:47 -0700

    initial commit: spring boot project setup

---
+++ src/main/java/dev/ethanz/speakle/SpeakleApplication.java
@@ -0,0 +1,14 @@
+package dev.ethanz.speakle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakleApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
