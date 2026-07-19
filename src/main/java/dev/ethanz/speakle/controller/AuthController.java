164cb975d30ffb1a80cbc3738ffe25a7d1426bbc
Ethan Zhang <dangerwindow@gmail.com>
2026-07-18 22:07:36 -0700

    Create register api

---
+++ src/main/java/dev/ethanz/speakle/controller/AuthController.java
@@ -0,0 +1,48 @@
+package dev.ethanz.speakle.controller;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.ethanz.speakle.dto.RegisterRequest;
+import dev.ethanz.speakle.entity.User;
+import dev.ethanz.speakle.repository.UserRepository;
+import dev.ethanz.speakle.service.JwtService;
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestParam RegisterRequest request) {
+        Optional<User> existingUser = userRepository.findByEmail(request.email());
+        if (existingUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        String hash = passwordEncoder.encode(request.password());
+        userRepository.save(new User(UUID.randomUUID().toString(), request.email(), hash));
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    
+}
