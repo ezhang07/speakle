ae3c63e02ee4e59f87d3561fb9ea11355e93c045
Ethan Zhang <dangerwindow@gmail.com>
2026-07-19 20:50:53 -0700

    Add table metadata to user entity

--- src/main/java/dev/ethanz/speakle/entity/User.java
+++ src/main/java/dev/ethanz/speakle/entity/User.java
@@ -5,9 +5,11 @@ import java.time.Instant;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Entity
+@Table(name = "users")
 @Getter
 public class User {
 
