0013f376b9311c3bc6b37baad191559fba9afbfe
Ethan Zhang <dangerwindow@gmail.com>
2026-07-19 20:51:38 -0700

    Add function to check validity of tokens

--- src/main/java/dev/ethanz/speakle/service/JwtService.java
+++ src/main/java/dev/ethanz/speakle/service/JwtService.java
@@ -24,7 +24,7 @@ public class JwtService {
 
     public String generateToken(User user) {
         Date now = new Date();
-        Date expiry = new Date(now.getTime() + 1000L * 60 * 60 * 24); 
+        Date expiry = new Date(now.getTime() + 1000L * 60 * 60 * 24);
 
         return Jwts.builder()
         .subject(user.getId()) // who the token identifies
@@ -32,5 +32,16 @@ public class JwtService {
         .expiration(expiry) // expiry date (24 hrs after)
         .signWith(key) // hash header+payload with secret, making the signature
         .compact(); // assemble into header.payload.signature string
+    }
+
+    // verify a token and pull the user id back out
+    // invalid/expired tokens are caught by this filter, and requests are rendered unauthenticatde
+    public String extractUserId(String token) {
+        return Jwts.parser()
+            .verifyWith(key) // recompute the signature with our secret and check it matches. If token is altered, this throws
+            .build()
+            .parseSignedClaims(token) // checks the expiration; expired token throws
+            .getPayload()
+            .getSubject(); // gets user id we stored in generateToken()
     }
 }
