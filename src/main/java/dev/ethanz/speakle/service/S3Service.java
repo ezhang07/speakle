550e905ff104024295e60d343cf1300d29c22cdf
Ethan Zhang <dangerwindow@gmail.com>
2026-08-07 14:23:59 -0700

    Create presignGetUrl method

--- src/main/java/dev/ethanz/speakle/service/S3Service.java
+++ src/main/java/dev/ethanz/speakle/service/S3Service.java
@@ -2,21 +2,27 @@ package dev.ethanz.speakle.service;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @Service
 public class S3Service {
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
     private final String bucket;
 
-    public S3Service(S3Client s3Client, @Value("${aws.s3.bucket}") String bucket) {
+    public S3Service(S3Client s3Client, S3Presigner s3Presigner, @Value("${aws.s3.bucket}") String bucket) {
         this.s3Client = s3Client;
+        this.s3Presigner = s3Presigner;
         this.bucket = bucket;
     }
 
@@ -30,5 +36,19 @@ public class S3Service {
                 .build();
 
         s3Client.putObject(request, RequestBody.fromFile(file));
+    }
+
+    public String presignGetUrl(String id) {
+        GetObjectRequest getRequest = GetObjectRequest.builder()
+        .bucket(bucket)
+        .key(id + ".webm")
+        .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+        .signatureDuration(Duration.ofMinutes(10)) // how long URL stays valid
+        .getObjectRequest(getRequest)
+        .build();
+
+        return s3Presigner.presignGetObject(presignRequest).url().toString();
     }
 }
